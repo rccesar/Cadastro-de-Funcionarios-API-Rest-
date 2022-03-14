438cd9987f209360a879dce716a7652120e52e5a
user <user@DESKTOP-PLSL3K0>
2022-03-14 20:20:57 -0300

    API REST usando Spring Boot

---
+++ userdept/src/main/java/com/springdemo/userdept/entities/User.java
@@ -0,0 +1,66 @@
+package com.springdemo.userdept.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+	private String nameString;
+	private String email;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "departament_id")
+	
+	private Departament departament;
+	
+	public User () {
+		
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNameString() {
+		return nameString;
+	}
+
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+
+	
+}
---
+++ userdept/src/main/java/com/springdemo/userdept/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.springdemo.userdept.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springdemo.userdept.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+	
+	
+
+}
